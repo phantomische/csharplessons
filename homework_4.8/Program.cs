3e78d98b9e08476b5a180e62859a4459658aaca7
phantomische <86479276+phantomische@users.noreply.github.com>
2022-01-19 20:49:55 +0500

    Add files via upload

---
+++ homework_4.8/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace homework_4._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
