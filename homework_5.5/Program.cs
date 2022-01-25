ebd685634a30e67f57c67cdd46b8d52b76f85b44
phantomische <sultan.yerbulatov@gmail.com>
2022-01-25 18:43:14 +0500

    Cannot complete exercise #1

--- homework_5.5/Program.cs
+++ homework_5.5/Program.cs
@@ -4,9 +4,28 @@ namespace homework_5._5
 {
     class Program
     {
+        string phrase = string.Empty;
+        string[] words = new string[];
+        static void StringToWords (string phrase, string[] words)
+        {
+            string phrase = "The quick brown fox jumps over the lazy dog.";
+            string[] words = phrase.Split(' ');
+        }
+        
+        static void PrintArray (string[] words)
+        {
+            foreach (var word in words)
+            {
+                Console.WriteLine($"<{word}>");
+            }
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            StringToWords();
+            PrintArray();
+            
         }
     }
 }
