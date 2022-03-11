fa0a22c24df5b1acb0758e8fc534da48b725c8cc
phantomische <sultan.yerbulatov@gmail.com>
2022-03-12 00:22:56 +0500

    homework question on Employee class

---
+++ homework_7.8/EmployeeRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_7._8
+{
+    class EmployeeRepository
+    {
+        int ID;
+        DateTime dateTime;
+        string fullName;
+        int age;
+        int height;
+        string birthdate;
+        string placeOfBirth;
+
+
+    }
+}
