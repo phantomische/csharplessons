7d07d3e42b7e97216133baa02f6880a0c4f5edb2
phantomische <sultan.yerbulatov@gmail.com>
2022-04-09 11:31:59 +0500

    question on task

--- homework_7.8/EmployeeRepository.cs
+++ homework_7.8/EmployeeRepository.cs
@@ -10,13 +10,12 @@ namespace homework_7._8
 {
     class EmployeeRepository
     {
-        // Список сотрудников (частный вспомогательный список)
+        // Список сотрудников
         private List<Employee> _employees { get; set; }
 
         public EmployeeRepository()
         {
             _employees = new List<Employee>();
-            _employees = GetEmployeesFromCsv();
         }
 
 
@@ -26,97 +25,118 @@ namespace homework_7._8
             return _employees;
         }
 
+        // Создаем сотрудника
+        public void Create(Employee employee)
+        {
+            _employees.Add(employee);
+        }
+
+        // Просмотри сотрудника
+        public void View(Employee employee)
+        {
+            var selectedEmployee = _employees.FirstOrDefault(e => e.ID == employee.ID);
+            Console.WriteLine(selectedEmployee.ID);
+        }
+        
         // Редактируем сотрудника
         public void Update(Employee employee)
         {
             var oldEmployee = _employees.FirstOrDefault(e => e.ID == employee.ID);
 
             _employees.Remove(oldEmployee);
-            _employees.Add(employee);
+            
         }
 
-        // Получаем список всех сотрудников из файла csv (парсинг файла, частный метод)
-        private List<Employee> GetEmployeesFromCsv()
+        // Удаляем сотрудника
+        public void Delete(Employee employee)
         {
-            var employees = new List<Employee>();
+            var oldEmployee = _employees.FirstOrDefault(e => e.ID == employee.ID);
 
-            using (TextFieldParser parser = new TextFieldParser(@"E:\employees.csv"))
-            {
-                parser.TextFieldType = FieldType.Delimited;
-                parser.SetDelimiters("	");
-                while (!parser.EndOfData)
-                {
-                    string[] props = parser.ReadFields();
-                    var employee = GetEmployeeFromCsvProps(props);
-                    employees.Add(employee);
-                }
-            }
-
-            return employees;
+            _employees.Remove(oldEmployee);
         }
 
-        // Получаем сотрудника из набора свойств csv строки (парсинг сотрудника, частный метод)
-        private Employee GetEmployeeFromCsvProps(string[] props)
-        {
-            int id = int.Parse(props[0]);
-            DateTime createdAt = DateTime.Parse(props[1]);
-            string fullName = props[2];
-            int age = int.Parse(props[3]);
-            int height = int.Parse(props[4]);
-            string birthDate = props[5];
-            string placeOfBirth = props[6];
+        //// Получаем список всех сотрудников из файла csv (парсинг файла, частный метод)
+        //private List<Employee> GetEmployeesFromCsv()
+        //{
+        //    var employees = new List<Employee>();
 
-            var employee = new Employee(
-                id,
-                createdAt,
-                fullName,
-                age,
-                height,
-                birthDate,
-                placeOfBirth);
+        //    using (TextFieldParser parser = new TextFieldParser(@"E:\employees.csv"))
+        //    {
+        //        parser.TextFieldType = FieldType.Delimited;
+        //        parser.SetDelimiters("	");
+        //        while (!parser.EndOfData)
+        //        {
+        //            string[] props = parser.ReadFields();
+        //            var employee = GetEmployeeFromCsvProps(props);
+        //            employees.Add(employee);
+        //        }
+        //    }
 
-            return employee;
-        }
+        //    return employees;
+        //}
 
-        public void WriteData()
-        {
-            string path = @"E:\employees.csv";
-            using (StreamWriter sw = new StreamWriter(path, true, Encoding.Unicode))
-            {
-                char key = 'y';
+        //// Получаем сотрудника из набора свойств csv строки (парсинг сотрудника, частный метод)
+        //private Employee GetEmployeeFromCsvProps(string[] props)
+        //{
+        //    int id = int.Parse(props[0]);
+        //    DateTime createdAt = DateTime.Parse(props[1]);
+        //    string fullName = props[2];
+        //    int age = int.Parse(props[3]);
+        //    int height = int.Parse(props[4]);
+        //    string birthDate = props[5];
+        //    string placeOfBirth = props[6];
 
-                do
-                {
-                    string note = string.Empty;
-                    Console.Write("\nВведите ID сотрудника: ");
-                    note += $"{Console.ReadLine()}\t";
+        //    var employee = new Employee(
+        //        id,
+        //        createdAt,
+        //        fullName,
+        //        age,
+        //        height,
+        //        birthDate,
+        //        placeOfBirth);
 
-                    string now = DateTime.Now.ToString();
-                    Console.WriteLine($"Дата и время добавления записи: {now}");
-                    note += $"{now}\t";
+        //    return employee;
+        //}
 
-                    Console.Write("Ф.И.О: ");
-                    note += $"{Console.ReadLine()}\t";
+        //public void WriteData()
+        //{
+        //    string path = @"E:\employees.csv";
+        //    using (StreamWriter sw = new StreamWriter(path, true, Encoding.Unicode))
+        //    {
+        //        char key = 'y';
 
-                    Console.Write("Возраст: ");
-                    note += $"{Console.ReadLine()}\t";
+        //        do
+        //        {
+        //            string note = string.Empty;
+        //            Console.Write("\nВведите ID сотрудника: ");
+        //            note += $"{Console.ReadLine()}\t";
 
-                    Console.Write("Рост: ");
-                    note += $"{Console.ReadLine()}\t";
+        //            string now = DateTime.Now.ToString();
+        //            Console.WriteLine($"Дата и время добавления записи: {now}");
+        //            note += $"{now}\t";
 
-                    Console.Write("Дата рождения: ");
-                    note += $"{Console.ReadLine()}\t";
+        //            Console.Write("Ф.И.О: ");
+        //            note += $"{Console.ReadLine()}\t";
 
-                    Console.Write("Место рождения: ");
-                    note += $"{Console.ReadLine()}\t";
-                    sw.WriteLine(note);
-                    Console.Write("Продолжить y/n"); key = Console.ReadKey(true).KeyChar;
-                } while (char.ToLower(key) == 'y');
-            }
-            Console.WriteLine();
-            Console.WriteLine($"Файл {path} успешно создан. Нажмите Enter для продолжения\n");
-            Console.ReadLine();
-        }
+        //            Console.Write("Возраст: ");
+        //            note += $"{Console.ReadLine()}\t";
+
+        //            Console.Write("Рост: ");
+        //            note += $"{Console.ReadLine()}\t";
+
+        //            Console.Write("Дата рождения: ");
+        //            note += $"{Console.ReadLine()}\t";
+
+        //            Console.Write("Место рождения: ");
+        //            note += $"{Console.ReadLine()}\t";
+        //            sw.WriteLine(note);
+        //            Console.Write("Продолжить y/n"); key = Console.ReadKey(true).KeyChar;
+        //        } while (char.ToLower(key) == 'y');
+        //    }
+        //    Console.WriteLine();
+        //    Console.WriteLine($"Файл {path} успешно создан. Нажмите Enter для продолжения\n");
+        //    Console.ReadLine();
+        //}
 
 
     }
--- homework_7.8/Program.cs
+++ homework_7.8/Program.cs
@@ -5,8 +5,9 @@ namespace homework_7._8
     class Program
     {
         static void Main(string[] args)
-        {
+        {    
             var employeeRepository = new EmployeeRepository();
+            var NewList = employeeRepository.Update(new Employee(6, DateTime.Now, "Putin Vladimir Bunkerovich", 70, 160, "1953", "Leningrad"));
            
 
             
