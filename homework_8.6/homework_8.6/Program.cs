d7c20cf16aa20ec15a69840b90aa23ab553df3be
phantomische <sultan.yerbulatov@gmail.com>
2022-04-20 21:02:22 +0500

    first exercise

---
+++ homework_8.6/homework_8.6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace homework_8._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            Display(numbers);
+            Console.WriteLine("\n");
+            Sort(numbers);
+        }
+
+        public static void Display(List<int> list)
+        {
+            Random random = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                list.Add(random.Next(100));
+            }
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static void Sort(List<int> list)
+        {
+            Random random = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                list.Add(random.Next(100));
+            }
+            var query = list.Where(x => x > 25 && x < 50).ToList();
+            foreach (var item in query)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
