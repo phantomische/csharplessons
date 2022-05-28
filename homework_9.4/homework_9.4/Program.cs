2878bc553c607e26074aa2ab4b20ac57475466ad
phantomische <sultan.yerbulatov@gmail.com>
2022-05-29 01:21:22 +0500

    problem with exercise

--- homework_9.4/homework_9.4/Program.cs
+++ homework_9.4/homework_9.4/Program.cs
@@ -1,60 +1,107 @@
-﻿using Newtonsoft.Json.Linq;
-using System;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Text;
+﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Types.InputFiles;
+using Telegram.Bot.Types.Enums;
+using System.Collections.Generic;
+using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bot.Requests;
 
 namespace homework_9._4
 {
-    public class Program
+    public static class Program
     {
-        static TelegramBotClient bot;
-        static void Main(string[] args)
+        static ITelegramBotClient bot = new TelegramBotClient("5320706394:AAGGNAuArVDSJV3ZEZQ_cv-y8wtHSbS7UdI");
+        public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
         {
-            string token = "hidden";
-            bot = new TelegramBotClient(token);
-            bot.OnMessage += MessageListener;
-            bot.StartReceiving();
-            Console.ReadKey();
-        }
-
-        private static void MessageListener(object sender, Telegram.Bot.Args.MessageEventArgs e)
-        {
-            string text = $"{DateTime.Now.ToLongTimeString()}: {e.Message.Chat.FirstName} {e.Message.Chat.Id} {e.Message.Text}";
-
-            Console.WriteLine($"{text} TypeMessage: {e.Message.Type.ToString()}");
- 
-
-            if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Document)
+            // Некоторые действия
+            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(update));
+            if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
             {
-                Console.WriteLine(e.Message.Document.FileId);
-                Console.WriteLine(e.Message.Document.FileName);
-                Console.WriteLine(e.Message.Document.FileSize);
-
-                DownLoad(e.Message.Document.FileId, e.Message.Document.FileName);
+                var message = update.Message;
+                if (message.Text.ToLower() == "/start")
+                {
+                    await botClient.SendTextMessageAsync(message.Chat, "Добро пожаловать на борт, добрый путник!");
+                    return;
+                }
+                await botClient.SendTextMessageAsync(message.Chat, "Привет-привет!!");
             }
-
-            if (e.Message.Text == null) return;
-
-            var messageText = e.Message.Text;
-
-            
-            bot.SendTextMessageAsync(e.Message.Chat.Id,
-                $"{messageText}"
-                );
         }
 
-        static async void DownLoad(string fileId, string path)
+        public static async Task<Message> SendDocumentAsync(
+        this ITelegramBotClient botClient,
+        ChatId chatId,
+        InputOnlineFile document,
+        InputMedia? thumb = default,
+        string? caption = default,
+        ParseMode? parseMode = default,
+        IEnumerable<MessageEntity>? captionEntities = default,
+        bool? disableContentTypeDetection = default,
+        bool? disableNotification = default,
+        bool? protectContent = default,
+        int? replyToMessageId = default,
+        bool? allowSendingWithoutReply = default,
+        IReplyMarkup? replyMarkup = default,
+        CancellationToken cancellationToken = default
+    ) =>
+        await botClient.ThrowIfNull(nameof(botClient))
+            .MakeRequestAsync(
+                request: new SendDocumentRequest(chatId, document)
+                {
+                    Thumb = thumb,
+                    Caption = caption,
+                    ParseMode = parseMode,
+                    CaptionEntities = captionEntities,
+                    DisableContentTypeDetection = disableContentTypeDetection,
+                    DisableNotification = disableNotification,
+                    ReplyToMessageId = replyToMessageId,
+                    AllowSendingWithoutReply = allowSendingWithoutReply,
+                    ReplyMarkup = replyMarkup
+                },
+                cancellationToken
+            )
+            .ConfigureAwait(false);
+
+
+        public static async Task<File> GetFileAsync(
+        this ITelegramBotClient botClient,
+        string fileId,
+        CancellationToken cancellationToken = default
+    ) =>
+        await botClient.ThrowIfNull(nameof(botClient))
+            .MakeRequestAsync(
+                request: new GetFileRequest(fileId),
+                cancellationToken: cancellationToken
+            )
+            .ConfigureAwait(false);
+
+        public static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
         {
-            var file = await bot.GetFileAsync(fileId);
-            FileStream fs = new FileStream("_" + path, FileMode.Create);
-            await bot.DownloadFileAsync(file.FilePath, fs);
-            fs.Close();
-             
-            fs.Dispose();
+            // Некоторые действия
+            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(exception));
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Запущен бот " + bot.GetMeAsync().Result.FirstName);
+
+            var cts = new CancellationTokenSource();
+            var cancellationToken = cts.Token;
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = { }, // receive all update types
+            };
+            bot.StartReceiving(
+                HandleUpdateAsync,
+                HandleErrorAsync,
+                receiverOptions,
+                cancellationToken
+            );
+            Console.ReadLine();
         }
     }
 }
