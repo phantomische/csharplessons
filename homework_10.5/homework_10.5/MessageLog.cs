06e5e1c41c8296fd9de9c58097c25c94b03b565c
phantomische <sultan.yerbulatov@gmail.com>
2022-06-28 21:07:02 +0500

    problem with exercise

---
+++ homework_10.5/homework_10.5/MessageLog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_10._5
+{
+    public struct MessageLog
+    {
+        public string Time { get; set; }
+
+        public long Id { get; set; }
+
+        public string Msg { get; set; }
+
+        public string FirstName { get; set; }
+
+        public MessageLog(string Time, string Msg, string FirstName, long Id)
+        {
+            this.Time = Time;
+            this.Msg = Msg;
+            this.FirstName = FirstName;
+            this.Id = Id;
+        }
+    }
+}
---
+++ homework_10.5/homework_10.5/TelegramMessageClient.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using Telegram.Bot;
+
+namespace homework_10._5
+{
+    public class TelegramMessageClient
+    {
+        private MainWindow w;
+
+        private TelegramBotClient bot;
+        public ObservableCollection<MessageLog> BotMessageLog { get; set; }
+
+        private void MessageListener(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            Console.WriteLine("---");
+            Debug.WriteLine("+++---");
+
+            string text = $"{DateTime.Now.ToLongTimeString()}: {e.Message.Chat.FirstName} {e.Message.Chat.Id} {e.Message.Text}";
+
+            Debug.WriteLine($"{text} TypeMessage: {e.Message.Type.ToString()}");
+
+            if (e.Message.Text == null) return;
+
+            var messageText = e.Message.Text;
+
+            w.Dispatcher.Invoke(() =>
+            {
+                BotMessageLog.Add(
+                new MessageLog(
+                    DateTime.Now.ToLongTimeString(), messageText, e.Message.Chat.FirstName, e.Message.Chat.Id));
+            });
+        }
+
+        public TelegramMessageClient(MainWindow W, string PathToken = "hidden")
+        {
+            this.BotMessageLog = new ObservableCollection<MessageLog>();
+            this.w = W;
+
+            bot = new TelegramBotClient(PathToken);
+
+            bot.OnMessage += MessageListener;
+
+            bot.StartReceiving();
+        }
+
+        public void SendMessage(string Text, string Id)
+        {
+            long id = Convert.ToInt64(Id);
+            bot.SendTextMessageAsync(id, Text);
+        }
+    }
+}
