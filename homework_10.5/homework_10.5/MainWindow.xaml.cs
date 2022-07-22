94ee56e0b00047aa3086f9a59ca8711b86cd65c6
phantomische <sultan.yerbulatov@gmail.com>
2022-07-22 21:34:59 +0500

    exercise check

--- homework_10.5/homework_10.5/MainWindow.xaml.cs
+++ homework_10.5/homework_10.5/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -20,20 +21,34 @@ namespace homework_10._5
     /// </summary>
     public partial class MainWindow : Window
     {
-        TelegramMessageClient client;
+        TelegramMsgClient client;
 
         public MainWindow()
         {
             InitializeComponent();
-
-            client = new TelegramMessageClient(this);
-
-            logList.ItemsSource = client.BotMessageLog;
+            client = new TelegramMsgClient(this);
+            logListBox.ItemsSource = TelegramMsgClient.BotMessageLog;
         }
 
-        private void btnMsgSendClick(object sender, RoutedEventArgs e)
+        private void refrshClck(object sender, RoutedEventArgs e)
         {
-            client.SendMessage(txtMsgSend.Text, TargetSend.Text);
+            logListBox.Items.Refresh();
+        }
+
+        private void sndBtnClck(object sender, RoutedEventArgs e)
+        {
+            client.SendMessage(txtMsgSend.Text, txtTarget.Text);
+        }
+
+        private void exitBtnClck(object sender, RoutedEventArgs e)
+        {
+            App.Current.MainWindow.Close();
+        }
+        private void exportSmsBtnClck(object sender, RoutedEventArgs e)
+        {
+            MessageLog.SerializeAllLogs(TelegramMsgClient.BotMessageLog);
+            logListBox.Items.Refresh();
+            MessageBox.Show("Сохранено", "Message Logging");
         }
     }
 }
