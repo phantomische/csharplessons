62233d9bf434d8deb0a2a7c461132da96bb4c8f5
phantomische <sultan.yerbulatov@gmail.com>
2022-08-15 22:52:34 +0500

    question on hometask

---
+++ homework_11.6/homework_11.6/Consultant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_11._6
+{
+    public class Consultant
+    {
+        public string LastName { get; }
+        public string FirstName { get; }
+        public string Patronomic { get; }
+        public string PhoneNumber { get; set; }
+        public string PassportDetails { get; } = string.Format("******************");
+            
+     }
+}
---
+++ homework_11.6/homework_11.6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace homework_11._6
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
